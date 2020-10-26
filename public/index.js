9677e0e3d861de625ce3abf53d35fbd938126830
Patrick Smith <patricksmith@Patricks-MacBook-Pro.local>
2020-10-25 23:20:37 -0400

    display db table data

--- public/index.js
+++ public/index.js
@@ -1,1 +1,33 @@
-console.log('test');
+
+console.log('Running index.js...');
+
+let url = 'http://localhost:3000/employees';
+
+function insertIntoDirectory() {
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			let table = document.getElementById("directory");
+			let row = table.insertRow(-1);
+
+			let id = row.insertCell(0);
+			let firstName = row.insertCell(1);
+			let lastName = row.insertCell(2);
+			let ext = row.insertCell(3);
+			let department = row.insertCell(4);
+			let email = row.insertCell(5);
+
+			data.forEach(employee => {
+				id.innerHTML = employee.id;
+				firstName.innerHTML = employee.first_name;
+				lastName.innerHTML = employee.last_name;
+				ext.innerHTML = employee.ext;
+				department.innerHTML = employee.department;
+				email.innerHTML = employee.email;
+			});
+		})
+		.catch(err => console.error(err));
+}
+
+insertIntoDirectory();
