189527f05871dfa4d9a1a7e65fda2f1091532973
Patrick Smith <patricksmith@Patricks-MacBook-Pro.local>
2020-10-25 23:29:47 -0400

    camelCase not underscores

--- public/index.js
+++ public/index.js
@@ -9,19 +9,20 @@ function insertIntoDirectory() {
 		.then(response => response.json())
 		.then(data => {
 			let table = document.getElementById("directory");
-			let row = table.insertRow(-1);
-
-			let id = row.insertCell(0);
-			let firstName = row.insertCell(1);
-			let lastName = row.insertCell(2);
-			let ext = row.insertCell(3);
-			let department = row.insertCell(4);
-			let email = row.insertCell(5);
 
 			data.forEach(employee => {
+				let row = table.insertRow(-1);
+
+				let id = row.insertCell(0);
+				let firstName = row.insertCell(1);
+				let lastName = row.insertCell(2);
+				let ext = row.insertCell(3);
+				let department = row.insertCell(4);
+				let email = row.insertCell(5);
+
 				id.innerHTML = employee.id;
-				firstName.innerHTML = employee.first_name;
-				lastName.innerHTML = employee.last_name;
+				firstName.innerHTML = employee.firstName;
+				lastName.innerHTML = employee.lastName;
 				ext.innerHTML = employee.ext;
 				department.innerHTML = employee.department;
 				email.innerHTML = employee.email;
